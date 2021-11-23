eccdf63fde1dcd111d7d8a7081aa9eaf55c01c86
Hser2bio <38903436+Hser2bio@users.noreply.github.com>
2021-11-23 16:48:12 +0100

    we dont like old clients

--- src/version.h
+++ src/version.h
@@ -24,7 +24,7 @@ static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70812;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70812;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
-static const int MIN_PEER_MNANNOUNCE = 70811;
+static const int MIN_PEER_MNANNOUNCE = 70812;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
